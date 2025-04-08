c81bc9b3a15b243c906e7b65fc38df43f664b477
Yifei Shao <jp2023213073@qmul.ac.uk>
2025-04-09 00:36:33 +0800

    Add files via upload

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/08/关于Cyrex/","2025/04/01/四月不是谎言/","2025/03/31/序言/","2025/03/25/开站首文/"];function toRandomPost(){
+var posts=["2025/04/01/四月不是谎言/","2025/03/31/序言/","2025/03/25/开站首文/","2025/03/25/测试文章/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
